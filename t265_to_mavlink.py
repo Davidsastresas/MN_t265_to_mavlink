a1a98883cfa73a4e277025c793112cb79c3d5011
DiegoJRAleixandre <diego.jimr@gmail.com>
2022-10-17 10:59:47 +0200

    Refactor GPIO workflow for easier reading

--- t265_to_mavlink.py
+++ t265_to_mavlink.py
@@ -28,7 +28,7 @@ import time
 import argparse
 import threading
 import signal
-import RPi.GPIO as GPIO
+import RPi.GPIO as gpio
 
 from time import sleep
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -55,6 +55,9 @@ relocalization = 0.0 #R2 relocalization enable option
 map_preservation = 0.0 #R2 map_preservation enable option
 #GPIO 
 mosfet = 16 #mosfet controlling t265 power
+gpio.setmode(gpio.BCM)
+gpio.setup(mosfet, gpio.OUT) #Set gpio20 of the BCM as output
+gpio.output(mosfet,gpio.LOW)
 
 # if no frames are received after this, reboot script
 wait_for_frames_timeout = 500
@@ -862,11 +865,9 @@ finally:
     conn.close()
     progress("INFO: Realsense pipeline and vehicle object closed.")
     #Now reboot camera using a GPIO to acces the mosfet before rebooting the script
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(mosfet, GPIO.OUT) #Set GPIO20 of the BCM as output
-    GPIO.output(mosfet,True)
+    gpio.output(mosfet,gpio.HIGH)
     time.sleep(0.1)
-    GPIO.output(mosfet,False)
-    GPIO.cleanup()
+    gpio.output(mosfet,gpio.LOW)
+    gpio.cleanup()
     #Finally exit
     sys.exit(exit_code)
